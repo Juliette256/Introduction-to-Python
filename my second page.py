a0025d89afb5e0e0f0c5f36d4e43c7ce2f62ffb4
Nakibuuka Juliet <julietandrewz@gmail.com>
2022-05-13 14:33:55 +0300

    dictionaries

--- my second page.py
+++ my second page.py
@@ -1,10 +1,13 @@
 import numbers
 from pickle import TRUE
+from re import M
+
+from pkg_resources import invalid_marker
  
 # EXERCISE
 def who_do_know():
   people=input('Name people you know: ')
-  people_list=people.strip()
+  people_list=people.strip().lower()
   return (people_list)
     
 def ask_user():
@@ -16,3 +19,47 @@ def ask_user():
     else:
      print('You are not registered')
 ask_user()
+
+# LISTS
+array_of_nu=[0,1,2,3,4]
+producted =[x+10 for x in range(5)]
+print(producted)
+
+# DICTIONARIES
+student ={
+  'name':'Jose',
+  'school':'computing',
+  'sgrades':(66,77,88)
+}
+
+def average_grade(data):
+  grades=data['sgrades']
+  return sum(grades) / len(grades)
+
+def average_grade(studentlist):
+  Total=0
+  Count=0
+  
+  for student in studentlist:
+    Total += sum(student['sgrades'])
+    Count +=len(student['sgrades'])
+    return Total / Count
+    
+
+class Store:
+  def __init__(self, name):
+    self.name=name
+    self.items=[]
+  
+  def add_item(self, itemname, itemprice):
+    self.iname=itemname
+    self.iprice=itemprice
+    items={
+       'name':itemname,
+       'price':itemprice
+          }
+    self.items.append(items)
+  
+  def stock_price(self):
+     total=sum([items['price'] for items in self.items])
+     return total
